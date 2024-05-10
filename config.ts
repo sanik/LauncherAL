7edc94cc5146d11062ffdbe502d17838670f13ef
Sio <sanik2021@fuix.net>
2024-05-10 14:02:52 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -7,7 +7,7 @@ export const window = {
     resizable: false,
     maximizable: false,
     fullscreenable: false,
-    title: 'Aurora Launcher',
+    title: 'Aurora Launcher 2',
 };
 
 export const api = {
